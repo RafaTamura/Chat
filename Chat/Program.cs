fd087f099fbb4bb7d2c5f775656cb3603cac34f5
Rafaela Tamura <rafaelatamura@gmail.com>
2024-02-11 02:20:54 -0300

    Commit Carnavalesco

---
+++ Chat/Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using OpenAI.Interfaces;
+
+namespace Chat
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IOpenAIService api = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = "OPENAI KEY"
+            });
+
+            Console.WriteLine("Como posso te ajudar?");
+            var pergunta = Console.ReadLine();
+
+            var completion = await api.ChatCompletion.CreateCompletion(
+                new ChatCompletionCreateRequest
+                {
+                    Messages = new List<ChatMessage>
+                    {
+                    ChatMessage.FromSystem("System message"),
+                    ChatMessage.FromUser(pergunta)
+                    },
+                    Model = Models.ChatGpt3_5Turbo0301
+                }) ;
+
+            if (completion.Successful)
+            {
+                Console.WriteLine(completion.Choices.FirstOrDefault()?.Message.Content);
+            }
+            else
+            {
+                Console.WriteLine(completion.Error?.Message);
+            }
+
+
+        }
+    }
+}
---
+++ Chat/README.md
@@ -0,0 +1,3 @@
+# Chat
+Esse foi um antigo projeto que encontrei no meu notebook, não queria perder o código então resolvi guardar ele aqui no GitHub
+Um simples Console que consome a API da OpenAI, com uma key válida você consegue utilizar o chat em uma console application, acredito que daria pra utilizar isso em diversas aplicações diferentes.
